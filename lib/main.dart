d8a1840d8b4c654d00ae4634163ef452c1e9b640
mianumar5653 <miaumar332@gmail.com>
2022-07-28 13:49:51 +0500

    backend start

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,24 @@ import 'package:concard/Views/screens/homeScreens/upgradeCompanyPremium/upgradeC
 import 'package:flutter/services.dart';
 import 'package:concard/Views/screens/intialScreens/splashScreen.dart';
 import 'package:flutter/material.dart';
+// import 'firebase_options.dart';
 
-void main() async {
-  // SystemChrome.setEnabledSystemUIOverlays([]);
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+Future<void> main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
   ));
-  // WidgetsFlutterBinding.ensureInitialized();
-  runApp(
-      MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: SplashScreen(),
-      )
-  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:  SplashScreen(),
+    );
+  }
 }
