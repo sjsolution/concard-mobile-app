38c0533bdedb296b93aea6c5983e3ea9d4c8ce39
mianumar5653 <miaumar332@gmail.com>
2022-10-20 19:09:51 +0500

    20 Oct

--- lib/Views/widgets/custome_profile_view_screen.dart
+++ lib/Views/widgets/custome_profile_view_screen.dart
@@ -658,11 +658,11 @@ class _CustomProfileViewScreenState extends State<CustomProfileViewScreen> {
                       children: [
                         GestureDetector(
                           onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (BuildContext context) =>
-                                        const RatingReviewScreen()));
+                            // Navigator.push(
+                            //     context,
+                            //     MaterialPageRoute(
+                            //         builder: (BuildContext context) =>
+                            //              RatingReviewScreen(userId: ,)));
                           },
                           child: Row(
                             children: [
